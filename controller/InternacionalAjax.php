5899780514496b749e38e2fb2255734d70c28875
Hugo Montes <hugomontes927@gmail.com>
2017-03-13 17:51:47 -0400

    Se ha iniciado la refactorizacion del modulo internacionales

--- controller/InternacionalAjax.php
+++ controller/InternacionalAjax.php
@@ -4,40 +4,84 @@ require_once("InternacionalController.php");
 $controller=new InternacionalController("../conexion/conexion.php");
 
 $proceso=$_REQUEST['proceso'];
-$indicador=$_REQUEST['indicador'];
 
-if($proceso==1){
-	$registros=$controller->getIndicadorByIdTabla($indicador);
+if($proceso=='buscaGrupo'){
+	$id=$_REQUEST['id'];
+	$registros=$controller->getAllGrupoByIdActividad($id);
+	$out='';
+	while ($reg=mysqli_fetch_array($registros)){
+		$out.="<option  value='".$reg['tabla']."'>".$reg['campos']."</option>";
+	}
+	echo $out;
+}elseif($proceso=='buscaDesagregacion'){
+	$tabla=$_REQUEST['tabla'];
+	$registros=$controller->getAllDesagregacionByNameTabla($tabla);
+	$out='';
+	while ($reg=mysqli_fetch_array($registros)){
+		$out.="<option  value='".$reg['desagregacion']."'>".$reg['desagregacion']."</option>";
+	}
+	echo $out;
+}elseif($proceso=='buscaMedicion'){
+	$tabla=$_REQUEST['tabla'];
+	$desagregacion=$_REQUEST['desagregacion'];
+	$registros=$controller->getAllMedicion($tabla,$desagregacion);
+	$out='';
+	while ($reg=mysqli_fetch_array($registros)){
+		$out.="<option  value='".$reg['medicion_indicador']."'>".$reg['medicion_indicador']."</option>";
+	}
+	echo $out;
+}elseif($proceso=='buscaCobertura'){
+	$tabla=$_REQUEST['tabla'];
+	$desagregacion=$_REQUEST['desagregacion'];
+	$medicion=$_REQUEST['medicion'];
+	$registros=$controller->getAllCobertura($tabla,$desagregacion,$medicion);
+	$out='';
+	while ($reg=mysqli_fetch_array($registros)){
+		$out.="<option  value='".$reg['C']."'>".$reg['C']."</option>";
+	}
+	echo $out;
+}elseif($proceso=='buscaIndicador'){
+	$tabla=$_REQUEST['tabla'];
+	$desagregacion=$_REQUEST['desagregacion'];
+	$medicion=$_REQUEST['medicion'];
+	$cobertura=$_REQUEST['cobertura'];
+	$registros=$controller->getAllIndicador($tabla,$desagregacion,$medicion,$cobertura);
 	$out='';
 	while ($reg=mysqli_fetch_array($registros)){
 		$out.="<option  value='".$reg['B']."'>".$reg['B']."</option>";
 	}
 	echo $out;
-}elseif($proceso==2){
-	$indicador2=$_REQUEST['indicador2'];
-	$registros=$controller->getCoberturasByIndicador($indicador,$indicador2);
-	$cobertura=array();
+}elseif($proceso=='buscaDescripcion'){
+	$tabla=$_REQUEST['tabla'];
+	$desagregacion=$_REQUEST['desagregacion'];
+	$medicion=$_REQUEST['medicion'];
+	$cobertura=$_REQUEST['cobertura'];
+	$indicador=$_REQUEST['indicador'];
+	$registros=$controller->getAllDescripcion($tabla,$desagregacion,$medicion,$cobertura,$indicador);
+	
+	$fila_indicador=array();
 	while ($reg=mysqli_fetch_array($registros)){
-		$cobertura[]=$reg['C'];
+		$fila_indicador[]=array('id'=>$reg['id'],'descripcion'=>$reg['descripcion']);
 	}
-	$ruta_xlsx=$controller->getRutaXlsx($indicador, $indicador2, $cobertura[0]);
+	$ruta_xlsx=$controller->getRutaXlsx($tabla,$indicador);
 	$data=array(
-		'cobertura' => $cobertura,
+		'indicador' => $fila_indicador,
 		'ruta_xlsx' => BASE_URL.$ruta_xlsx,
 	);
 	header('Content-type: application/json; charset=utf-8');
     echo json_encode($data,JSON_NUMERIC_CHECK);
     exit();	
-}elseif($proceso==3){
-	$departamental=$_REQUEST['departamental'];
-	$indicador2=$_REQUEST['indicador2'];
-	$registros=$controller->getDescripcionByIndicador($indicador,$departamental,$indicador2);
+}elseif($proceso=='buscaPeriodicidad'){
+	$tabla=$_REQUEST['tabla'];
+	$id=$_REQUEST['id'];
+	$registros=$controller->getPeriodos($tabla, $id);
 	$out='';
-	while ($reg=mysqli_fetch_array($registros)){
-		$out.="<option  value='".$reg['DESCRIPCION']."'>".$reg['DESCRIPCION']."</option>";
+	foreach ($registros as $key => $value) {
+		$out.="<option  value='".$key."'>".$value."</option>";
 	}
 	echo $out;
-}elseif($proceso==4){
+	/*
+	$indicador=$_REQUEST['indicador'];
 	$descripcion=$_REQUEST['descripcion'];
 	$departamental=$_REQUEST['departamental'];
 	$indicador2=$_REQUEST['indicador2'];
@@ -48,23 +92,28 @@ if($proceso==1){
 		$out.="<option  value='".$key."'>".$value."</option>";
 	}
 	echo $out;
-}elseif($proceso==5){
+	*/
+}elseif($proceso=='imprimirResultados'){
+	$tabla=$_REQUEST['tabla'];
+	$id=$_REQUEST['id'];
 	$ini=$_REQUEST['ini'];
 	$fin=$_REQUEST['fin'];
+	/*
 	$descripcion=$_REQUEST['descripcion'];
 	$departamental=$_REQUEST['departamental'];
 	$indicador2=$_REQUEST['indicador2'];
-
-	$id=$controller->getIdIndicador($indicador,$departamental,$indicador2,$descripcion);
+	*/
+	//$id=$controller->getIdIndicador($indicador,$departamental,$indicador2,$descripcion);
 	
-	$gestion=$controller->getNombreColumnas($id,$indicador,$ini,$fin);
-	$series=$controller->getSerie($id,$indicador,$ini,$fin);
+	$gestion=$controller->getNombreColumnas($id,$tabla,$ini,$fin);
+	$series=$controller->getSerie($id,$tabla,$ini,$fin);
 	
-	$reg_indicador=$controller->getIndicadorById($id,$indicador);
+	$reg_indicador=$controller->getIndicadorById($id,$tabla);
 	$descripcion=$reg_indicador['DESCRIPCION'];
-	$cobertura=$reg_indicador['C'];
+	// $cobertura=$reg_indicador['C'];
 	$titulo=array('text'=>$reg_indicador['B']);
-	$subtitulo=array('text'=>$cobertura);
+	// $subtitulo=array('text'=>$cobertura);
+	$subtitulo=array('text'=>$descripcion);
 	$tituloy=array('text'=>$reg_indicador['D']);
 
 	// GENERAR SERIE
@@ -85,6 +134,7 @@ if($proceso==1){
 	$char=array(
 		'titulo'=>$titulo,
 		'subtitulo'=>$subtitulo,
+		//'subtitulo'=>$descripcion,
 		'serie'=>$serie,
 		'categorias'=>$categorias,
 		'tituloy'=>$tituloy,
@@ -92,5 +142,6 @@ if($proceso==1){
 	);
 	header('Content-type: application/json; charset=utf-8');
     echo json_encode($char,JSON_NUMERIC_CHECK);
-    exit();	
+    exit();
 }
+
