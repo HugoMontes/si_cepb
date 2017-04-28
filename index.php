46ec1f27213767fab6331e835d92990e13ce22a0
Felix Montes <hugomontes927@gmail.com>
2017-04-28 08:35:06 -0400

    Cambios 28042017 Ventana inicial del sistema

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php require_once("config/config.php"); ?>
 <?php require_once("pages/template/header.php"); ?>
+<style>
+	.main-container{
+		font-size: 18px;
+	    line-height: 38px;
+	    text-align: justify;
+	}
+	.main-container p.pie-main{
+		background: #004a84;
+	    color: white;
+	    padding-left: 13px;
+	    text-align: center;
+	}
+</style>
+<div class="container main-container">
+	<p>La Confederación de Empresarios Privados de Bolivia, con el apoyo de la ACT/EMP de la OIT, ha desarrollado un nuevo servicio; denominado “Sistema de Información Económica (SIE)” que ofrece a sus gremios regionales y sectoriales y, a través de ellos a las empresas; información económica de fuentes oficiales, organizada de una manera amigable, dinámica y rápida, bajo la premisa de que la información constituye uno de los principales insumos para la toma de decisiones; que permite prevenir futuros contingentes y principalmente establecer medidas adecuadas ante situaciones adversas o aprovechar competitivamente las coyunturas positivas.</p>
+
+	<p>El SIE presenta información clasificada en tres grandes grupos:</p>
+	
+	<ul>
+		<li><strong>Indicadores Históricos,</strong> referida a información principalmente en periodos anuales.</li>
+		<li><strong>Indicadores de Coyuntura,</strong> referida a información de períodos más cortos que un año y índices de distintas actividades.</li>
+		<li><strong>Indicadores Internacionales,</strong> básicamente contempla información del Doing Business y Foro de Competitividad, para los países de Latinoamérica.</li>
+	</ul>
+
+	<p>Así mismo brinda la posibilidad de realizar combinaciones de indicadores según las siguientes opciones:</p>
+
+	<ul>
+		<li><strong>Combinación de Indicadores,</strong> permite relacionar los mismos, en base a predeterminados indicadores.</li>
+		<li><strong>Comparativa de Indicadores,</strong> permite comparar dos indicadores de forma simultánea.</li>
+	</ul>
+	<p>Y, finalmente contiene un <strong>Glosario Estadístico,</strong> el cual brinda al usuario una gama conceptos más utilizados en estadísticas.</p>
+	<p>El SIE al ser una plataforma amigable, es de fácil uso.</p>
+
+	<p class="pie-main">2017 SIE - CEPB | Con el apoyo de ACT/EMP de la OIT</p>
+</div>
 <?php require_once("pages/template/footer.php"); ?>
